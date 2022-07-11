a2f00f3376d0dde97fe3d68148f72011917e28b3
Jessica Kimbrell <dfn7vs@virginia.edu>
2022-07-11 13:43:11 -0400

    Added Setup files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='Booklover',
+      version='0.1',
+      description='A booklover package',
+      url='https://github.com/kimbrellj/M14_HW_Package',
+      author='Jessica Kimbrell',
+      author_email='dfn7vs@virginia.edu',
+      license='MIT',
+      packages=['booklover'])
